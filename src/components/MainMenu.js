3575a96e1f7f70886514838daf02d742eaedda13
3ndrius <andrzejo1991@gmail.com>
2018-07-17 10:01:07 +0200

    Adding menu file

---
+++ src/components/MainMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { Link } from 'gatsby-link';
+
+export default class MainMenu extends Component {
+  render() {
+    return (
+      <div>
+          <h1> Menu </h1>
+        
+      </div>
+    )
+  }
+};
