02f0e128693eaf751b3ae6e000bee8ef3f49077e
3ndrius <andrzejo1991@gmail.com>
2018-07-18 21:46:37 +0200

    Fix v3 node and pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,94 +1,3 @@
-// const _ = require(`lodash`);
-// const Promise = require(`bluebird`);
-// const path = require(`path`);
-// const slash = require(`slash`);
-
-// const queryAll = require(`./src/queries/queryAll.js`);
-
-// const pageQuery = `
-// {
-//   allWordpressPage {
-//     edges {
-//       node {
-//         id
-//         slug
-//         status
-//         template
-//       }
-//     }
-//   }
-// }
-// `
-
-// const postsQuery = `
-// {
-//   allWordpressPost {
-//     edges {
-//       node {
-//         id
-//         slug
-//         status
-//         template
-//         format
-//       }
-//     }
-//   }
-// }
-// `
-
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//     const { createPage } = boundActionCreators;
-
-
-//     return new Promise((resolve, reject) => {
-
-//         // Pages
-//         graphql(queryAll)
-//             .then(result => {
-//                 if (result.errors) {
-//                     console.log(result.errors);
-//                     reject(result.errors);
-//                 }
-
-//                 const pageTemplate = path.resolve("./src/templates/page.js");
-
-//                 _.each(result.data.allWordpressPage.edges, edge => {
-//                     createPage({
-//                         path: `/${edge.node.slug}/`,
-//                         component: slash(pageTemplate),
-//                         context: {
-//                             id: edge.node.id,
-//                         },
-//                     });
-//                 });
-//             })
-
-//             .then(() => {
-//                 graphql(queryAll)
-//                     .then(result => {
-//                         if (result.errors) {
-//                             console.log(result.errors);
-//                             reject(result.errors);
-//                         }
-//                         const postTemplate = path.resolve("./src/templates/post.js");
-
-//                         _.each(result.data.allWordpressPost.edges, edge => {
-//                             createPage({
-//                                 path: `/post/${edge.node.slug}/`,
-//                                 component: slash(postTemplate),
-//                                 context: {
-//                                     id: edge.node.id,
-//                                 },
-//                             });
-//                         });
-//                         resolve();
-//                     });
-//             });
-//         // ==== END POSTS ====
-//     });
-// };
-
 const _ = require(`lodash`)
 const Promise = require(`bluebird`)
 const path = require(`path`)
@@ -125,11 +34,11 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
                 const posts = result.data.allWordpressPost.edges
 
                 createPaginatedPages({
-                    edges: result.data.posts.edges,
+                    edges: posts,
                     createPage: createPage,
-                    pageTemplate: "scr/templates/posts.js",
-                    pageLength: 6,
-                    pathPrefix:"posts"
+                    pageTemplate: "src/templates/posts.js",
+                    pageLength: 3,
+                    pathPrefix: "posts"
                 })
 
                 posts.forEach(edge => {
